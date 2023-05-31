e40955db9ad07bbd4fbe848aa4ead1ff7a7847d3
maxregnerisch <113422283+maxregnerisch@users.noreply.github.com>
2023-05-31 13:42:19 -0600

    Update remix.sh

--- build/remix.sh
+++ build/remix.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# Update the environment
+# Upgrade the system
 apt full-upgrade -y
 
-# Add universe and multiverse.
+# Add universe and multiverse repositories
 add-apt-repository -y --no-update universe
 add-apt-repository -y --no-update multiverse
 apt update -y
@@ -12,7 +12,7 @@ apt install -y vanilla-gnome-desktop vanilla-gnome-default-settings
 apt install -y -f
 apt purge -y ubuntu-desktop ubuntu-session
 
-# Remove pre-installed snap stuff to prevert issues while building
+# Remove pre-installed snap packages to prevent issues while building
 snap remove --purge firefox
 snap remove --purge snap-store
 snap remove --purge gnome-3-28-2004
@@ -27,5 +27,5 @@ apt install -y flatpak gnome-software gnome-software-plugin-flatpak
 apt-mark hold snapd
 flatpak remote-add --system flathub https://flathub.org/repo/flathub.flatpakrepo
 
-# Install Gnome web browser (epiphany) insted of default Firefox snap
+# Install Gnome web browser (epiphany) instead of default Firefox snap
 sudo apt install -y epiphany-browser
